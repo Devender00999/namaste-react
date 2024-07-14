f14ca8e453896a884b3eacdc207abccb24ee00c5
Devender Kumar <devender00999@gmail.com>
2024-07-14 12:30:40 +0530

    Sibling elements in react

--- App.js
+++ App.js
@@ -8,15 +8,17 @@
  *
  */
 
-const parent = React.createElement(
-   "div",
-   { id: "parent" },
-   React.createElement(
-      "div",
-      { id: "child" },
-      React.createElement("h1", { id: "heading" }, "Hello World from React!!")
-   )
-);
+const parent = React.createElement("div", { id: "parent" }, [
+   React.createElement("div", { id: "child" }, [
+      React.createElement("h1", { id: "heading", key: 1 }, "Hello World from React!!"),
+      React.createElement("h2", { id: "heading2", key: 2 }, "Namaste!"),
+   ]),
+   ,
+   React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", { id: "heading", key: 1 }, "Hello World from React!!"),
+      React.createElement("h2", { id: "heading2", key: 2 }, "Namaste!"),
+   ]),
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
