b6ba1f22599d5ebdb02248830657f81666981928
Devender Kumar <devender00999@gmail.com>
2024-07-17 20:09:59 +0530

    Added react as a package

--- App.js
+++ App.js
@@ -1,16 +1,9 @@
-/*
- *
- * <div id="parent">
- *    <div id="child">
- *       <h1>I'm h1 tag</h1>
- *    </div>
- * </div>
- *
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement("div", { id: "parent" }, [
    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", { id: "heading", key: 1 }, "Hello World from React!!"),
+      React.createElement("h1", { id: "heading", key: 1 }, "Hello World from React! 🚀"),
       React.createElement("h2", { id: "heading2", key: 2 }, "Namaste!"),
    ]),
    ,
