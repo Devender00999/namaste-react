a05538e08b5730cda888875c7904d9c4b6226998
Devender Kumar <devender00999@gmail.com>
2024-07-31 22:23:27 +0530

    Added Header using components

--- App.js
+++ App.js
@@ -1,30 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./app.css";
 
-// JSX - HTML like or xml like syntax
-const jsxHeading = (
-   <h1 className="heading" id="hello">
-      Namste React using JSX 🚀{" "}
-   </h1>
-);
+/* 
+   Header 
+      - Logo
+      - Nav Items
+   Body
+      - Search bar
+      - RestaurantContainer
+         - RestaurantCard
+   Footer
+      - Copyright
+      - Links
+      - Address
+      - Contact
+*/
 
-// React funcitonal component
-const Title = (d) => {
-   console.log(d);
-   return <h1>Title 🚀</h1>;
-};
-
-const Heading = () => {
-   return React.createElement("h1", {}, "Hello World Heading.");
-};
-
-const Container = () => {
+const Header = () => {
    return (
-      <div>
-         {jsxHeading}
-         {Title((d = "hello world"))}
-         <Title />
-         <Heading />
+      <nav className="header">
+         <div className="logo-container">
+            <img
+               className="logo"
+               src="https://www.logodesign.net/logo/smoking-burger-with-lettuce-3624ld.png"
+            />
+         </div>
+         <ul className="nav-container">
+            <li className="nav-item">Home</li>
+            <li className="nav-item">About Us</li>
+            <li className="nav-item">Contact Us</li>
+            <li className="nav-item">Cart</li>
+         </ul>
+      </nav>
+   );
+};
+
+const AppLayout = () => {
+   return (
+      <div className="app">
+         <Header></Header>
       </div>
    );
 };
@@ -32,4 +47,4 @@ const Container = () => {
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // it will convert react element object to html element
-root.render(<Container />);
+root.render(<AppLayout />);
