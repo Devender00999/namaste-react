94d266619d64754fc909e6416ade5dfec51f888a
Devender Kumar <devender00999@gmail.com>
2024-08-11 11:46:35 +0530

    Added dynamic data and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: [
+      "./src/**/*.{js,jsx,ts,tsx}", // Files in src directory
+      "./*.{js,jsx,ts,tsx}", // Files in the root directory
+   ],
+   theme: {
+      extend: {},
+   },
+   plugins: [],
+};
