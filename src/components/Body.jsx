e0cb7cf493e59dc8a88720935b3930a11c8996d7
Devender Kumar <devender00999@gmail.com>
2024-08-15 17:57:52 +0530

    Added search bar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,24 +1,55 @@
 import RestaurantCard from "./RestaurantCard";
 import { restarurantData } from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
    const [listOfRestarurants, setListOfRestaurant] = useState(restarurantData);
+   const [query, setQuery] = useState("");
+
+   useEffect(() => {
+      if (query.length !== 0) {
+         const filtered = listOfRestarurants.filter((item) => {
+            return item.info.name.toLowerCase().includes(query.toLowerCase());
+         });
+         setListOfRestaurant(filtered);
+      } else {
+         setListOfRestaurant(restarurantData);
+      }
+   }, [query]);
+
    return (
       <div className="body">
-         <div className="m-5 button">
-            <button
-               type="button"
-               onClick={() => {
-                  const filteredList = listOfRestarurants.filter(
-                     (item) => item.info.avgRating > 4.2
-                  );
-                  setListOfRestaurant(filteredList);
-               }}
-               className="btn btn-primary"
-            >
-               Top Rated Restaurant
-            </button>
+         <div className="m-5 flex justify-between gap-4">
+            <div>
+               <button
+                  type="button"
+                  onClick={() => {
+                     const filteredList = listOfRestarurants.filter(
+                        (item) => item.info.avgRating > 4.2
+                     );
+                     setListOfRestaurant(filteredList);
+                  }}
+                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+               >
+                  Top Rated Restaurant
+               </button>
+
+               <button
+                  className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700"
+                  onClick={() => setListOfRestaurant(restarurantData)}
+               >
+                  Clear
+               </button>
+            </div>
+
+            <div>
+               <input
+                  value={query}
+                  onChange={(e) => setQuery(e.target.value)}
+                  type="search"
+                  className="border-[1px] border-gray-400 rounded-md p-1 outline-none"
+               />
+            </div>
          </div>
          <div className="res-container">
             {listOfRestarurants.map((restaurant) => (
