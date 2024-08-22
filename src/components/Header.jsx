08979b726f193c056a0e464ae1b804e453e41540
Devender Kumar <devender00999@gmail.com>
2024-08-22 10:02:36 +0530

    Added Header image

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constants";
 
 const Header = () => {
+   const [loginBtn, setLoginBtn] = useState("Login");
+   console.log("Header rendered");
    return (
       <nav className="header">
          <div className="logo-container">
@@ -11,6 +14,12 @@ const Header = () => {
             <li className="nav-item">About Us</li>
             <li className="nav-item">Contact Us</li>
             <li className="nav-item">Cart</li>
+            <li
+               className="nav-item"
+               onClick={() => setLoginBtn((prev) => (prev === "Login" ? "Logout" : "Login"))}
+            >
+               {loginBtn}
+            </li>
          </ul>
       </nav>
    );
