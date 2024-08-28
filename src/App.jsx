cc7b80d170326bea45aec311b261114578f5bb29
Devender Kumar <devender00999@gmail.com>
2024-08-28 21:18:03 +0530

    Added react-router-dom for adding routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import "./components/Header";
 import "./app.css";
 import Header from "./components/Header";
 import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 
 const AppLayout = () => {
    return (
@@ -14,7 +18,13 @@ const AppLayout = () => {
    );
 };
 
+const router = createBrowserRouter([
+   { path: "/", element: <AppLayout />, errorElement: <Error /> },
+   { path: "/about", element: <About /> },
+   { path: "/contact", element: <Contact /> },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 // it will convert react element object to html element
-root.render(<AppLayout />);
+root.render(<RouterProvider router={router} />);
