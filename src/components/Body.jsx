781448f883219c07e23a88cfe0ccd2c14690841f
Devender Kumar <devender00999@gmail.com>
2024-10-15 10:11:31 +0530

    Added tailwind form plugin

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -56,7 +56,8 @@ const Body = () => {
                         }}
                         type="text"
                         id="first_name"
-                        className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                        // className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 "
+                        className="form-input rounded"
                         placeholder="Enter restaurant name"
                         required
                      />
